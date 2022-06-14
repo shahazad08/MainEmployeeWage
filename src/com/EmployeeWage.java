7cb90facea9268e8e8c685f0199e75f7bd8e3d04
shahazad08 <sk.shahazad@gmail.com>
2022-06-14 16:54:56 +0530

    Added: Full Time wage Calculated

--- src/com/EmployeeWage.java
+++ src/com/EmployeeWage.java
@@ -5,15 +5,22 @@ import java.util.Random;
 public class EmployeeWage {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Computation");
+        int EMP_RATE_PER_HOUR = 20;
+        int empHours = 0;
+        int empWage = 0;
         int IS_FULL_TIME = 1;
         Random random = new Random();
         int empCheck = random.nextInt(2);
         if (empCheck == IS_FULL_TIME){
             System.out.println("Employee is Present");
+            empHours = 8;
         }
         else {
             System.out.println("Employee is Absent");
+            empHours = 0;
         }
+        empWage = empHours * EMP_RATE_PER_HOUR;
+        System.out.println("The daily wages of an employee is : " + empWage);
     }
     }
 
