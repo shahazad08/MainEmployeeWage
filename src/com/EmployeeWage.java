a448b32daf169d17d22481734285eb12374fd202
shahazad08 <sk.shahazad@gmail.com>
2022-06-14 16:44:52 +0530

    Added:Emplyee Attedance check funcionality added

--- src/com/EmployeeWage.java
+++ src/com/EmployeeWage.java
@@ -1,8 +1,19 @@
 package com;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Computation");
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if (empCheck == IS_FULL_TIME){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
-}
+    }
 
